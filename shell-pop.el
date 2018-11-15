1bc0d06ecb6e49aff1a590b3305d6c7c359f0d53
Thomas de Beauchêne <thomas.de.beauchene@sap.com>
2018-11-15 13:09:36 +0100

    add customize variable to cleanup buffer at process exit

--- shell-pop.el
+++ shell-pop.el
@@ -154,6 +154,9 @@ effect when `shell-pop-window-position' value is \"full\"."
   :type 'boolean
   :group 'shell-pop)
 
+(defcustom shell-pop-cleanup-buffer-at-process-exit t
+  "If non-nil, cleanup the shell's buffer after its process exits.")
+
 (defun shell-pop--set-universal-key (symbol value)
   (set-default symbol value)
   (when value (global-set-key (read-kbd-macro value) 'shell-pop))
@@ -266,6 +269,8 @@ The input format is the same as that of `kbd'."
          process
          (lambda (_proc change)
            (when (string-match-p "\\(?:finished\\|exited\\)" change)
+             (when shell-pop-cleanup-buffer-at-process-exit
+               (kill-buffer))
              (if (one-window-p)
                  (switch-to-buffer shell-pop-last-buffer)
                (delete-window)))))))))
